1ef97d9c6f16c6ca98d4dc9a7a88be273734ae68
ottogibbon <113352402+ottogibbon@users.noreply.github.com>
2022-11-17 11:53:11 +0000

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,10 @@
+tuple1 = ("Carroll", 12.1, "13.4", "10.4", "11.4")
+list1 = ["Carroll", "12.1", "13.4", "10.4", "11.4"]
+print(tuple1[1] * 2)
+print(tuple1[0])
+print(list1[0])
+tuple2 = ("Carroll")
+list2 = ["Carroll"]
+print(list2[0])
+print(tuple2[1])
+print(input())
